6be7627805bd73aee688d369012ed8fa07e78a0f
snrn2426@kpu.ac.kr <jhltk2426@naver.com>
2018-10-21 16:03:11 +0900

    21 나무 철봉 + 벤치 프레스 완료
    런닝머신 해야함

--- 실습/GL_1(11~)/GL_1(11~)/21.cpp
+++ 실습/GL_1(11~)/GL_1(11~)/21.cpp
@@ -23,6 +23,7 @@ void Draw_bottom();
 void Obj_animation();
 void Draw_Tree();
 void Draw_horizontal();
+void Draw_bench_press();
 
 class Angle {
 public:
@@ -43,6 +44,9 @@ float tree_size = 1.f;
 
 ANIMATION horizontal_animation = increase;
 float horizontal_human_radian_x = 0.f;
+
+ANIMATION bench_press_animation = increase;
+float bench_press_human_radian_x = 0.f;
 
 void main(int argc, char **argv) {
 	glutInit(&argc, argv);
@@ -86,6 +90,7 @@ GLvoid drawScene(GLvoid) {
 	
 	Draw_Tree();
 	Draw_horizontal();
+	Draw_bench_press();
 
 	glPopMatrix();
 
@@ -265,6 +270,15 @@ void Obj_animation() {
 		horizontal_human_radian_x -= 3.f;
 		if (horizontal_human_radian_x < -45.f)	horizontal_animation = increase;
 	}
+
+	if (bench_press_animation == increase) {
+		bench_press_human_radian_x += 3.f;
+		if (bench_press_human_radian_x > 45.f)	bench_press_animation = decrease;
+	}
+	else if (bench_press_animation == decrease) {
+		bench_press_human_radian_x -= 3.f;
+		if (bench_press_human_radian_x < 0.f)	bench_press_animation = increase;
+	}
 }
 
 void Draw_Tree() {
@@ -377,6 +391,143 @@ void Draw_horizontal() {
 	glPopMatrix(); // »ç¶÷
 	
 	glPopMatrix(); // »ç¶÷ È¸Àü ±âÁØ
+
+	glPopMatrix(); // x_z ÁÂÇ¥ÀÌµ¿
+}
+
+void Draw_bench_press() {
+	glPushMatrix(); // x_z ÁÂÇ¥ÀÌµ¿
+	glTranslatef(220.f, 0.f, 160.f);
+	glPushMatrix(); // º¥Ä¡
+
+	glPushMatrix(); // ±âµÕ
+	glTranslatef(0.f, 50.f, 0.f);
+	glScalef(10.f, 100.f, 10.f);
+	glColor4f(0.2f, 0.2f, 0.8f, 1.f);
+	for (float i = -5.f; i < 10.f; i += 10.f) {
+		glPushMatrix(); //
+		glTranslatef(0.f, 0.f, i);
+		glutSolidCube(1);
+		glPopMatrix(); //
+	}
+	glPopMatrix(); // ±âµÕ
+
+	glPushMatrix(); // ÀÛÀº±âµÕ
+	glTranslatef(-100.f, 20.f, 0.f);
+	glScalef(10.f, 40.f, 10.f);
+	glColor4f(0.2f, 0.2f, 0.8f, 1.f);
+	for (float i = -4.5f; i < 10.f; i += 9.f) {
+		glPushMatrix(); //
+		glTranslatef(0.f, 0.f, i);
+		glutSolidCube(1);
+		glPopMatrix(); //
+	}
+	glPopMatrix(); // ÀÛÀº±âµÕ
+
+	glTranslatef(-40.f,30.f,0.f);
+	glScalef(120.f,10.f,100.f);
+	glutSolidCube(1);
+	glPopMatrix(); // º¥Ä¡
+
+	glPushMatrix(); // »ç¶÷
+	glTranslatef(25.f, 43.f, 0.f);
+	glRotatef(180.f,0.f,1.f,0.f);
+	glRotatef(90.f, 0.f, 0.f, 1.f);
+	
+	glPushMatrix(); // ÆÈ
+	glColor4f(1.f, 0.8f, 0.6f, 1.f);
+	glRotatef(-90.f, 0.f, 0.f, 1.f);
+	glRotatef(-180.f, 0.f, 1.f, 0.f);
+	glTranslatef(-40.f, -15.f, 0.f);
+	for (float i = -30.f; i < 60.f; i += 60.f) {
+		glPushMatrix(); //ÆÈ
+		glTranslatef(0.f,15.f,i);
+		if (i <= -30.f)	glRotatef(180.f, 0.f, 1.f, 0.f);
+		glRotatef(bench_press_human_radian_x, 1.f, 0.f,0.f);
+		glPushMatrix();//¾Æ·¡ÆÈ
+		glScalef(10.f, 30.f, 10.f);
+		glutSolidCube(1);
+		glPopMatrix();//¾Æ·¡ÆÈ
+		glTranslatef(0.f, 15.f, 0.f);
+		glPushMatrix(); //À­ÆÈ
+		glRotatef(-bench_press_human_radian_x*2, 1.f, 0.f, 0.f);
+		glTranslatef(0.f, 15.f, 0.f);
+		glScalef(10.f, 30.f, 10.f);
+		glutSolidCube(1);
+		glPopMatrix(); //À­ÆÈ
+		glPopMatrix(); //ÆÈ
+	}
+	
+	glPushMatrix(); // ¿ª±â
+	glTranslatef(0.f, 60.f - 20 * sin(deg(bench_press_human_radian_x)), 0.f);
+	glPushMatrix(); // ¼Õ
+	glColor4f(1.f, 0.8f, 0.6f, 1.f);
+	for (float i = -30.f; i < 60.f; i += 60.f) {
+		glPushMatrix();
+		glTranslatef(0.f, 0.f, i);
+		glScalef(10.f, 10.f, 10.f);
+		glutSolidCube(1);
+		glPopMatrix();
+
+	}
+	glPopMatrix(); // ¼Õ
+
+	glPushMatrix(); // ºÀ
+	glColor4f(0.f, 0.f, 0.f, 0.f);
+	glScalef(5.f, 5.f, 150.f);
+	glutSolidCube(1);
+	glPopMatrix(); // ºÀ
+
+	for (float i = -1.f; i < 2.f; i += 2.f) {
+		glPushMatrix();
+		glTranslatef(0.f, 0.f, i * 75.f);
+		glColor4f(0.7f, 0.f, 0.7f, 0.f);
+		glutSolidCylinder(20.f, 5.f, 15, 15);
+		glTranslatef(0.f, 0.f, i * 5.f);
+		glColor4f(0.f, 0.7f, 0.7f, 0.f);
+		glutSolidCylinder(15.f, 5.f, 15, 15);
+		glTranslatef(0.f, 0.f, i * 5.f);
+		glColor4f(0.7f, 0.7f, 0.f, 0.f);
+		glutSolidCylinder(10.f, 5.f, 15, 15);
+		glPopMatrix();
+	}
+
+	glPopMatrix(); // ¿ª±â
+	glPopMatrix(); // ÆÈ
+
+
+	glPushMatrix(); // 1
+	glTranslatef(0.f, -20.f, 0.f);
+	glColor4f(0.f, 0.f, 0.f, 0.f);
+	glutSolidSphere(20.f, 10.f, 10.f);
+	glTranslatef(10.f, -5.f, 0.f);
+	glColor4f(1.f, 0.8f, 0.6f, 1.f);
+	glutSolidSphere(15.f, 10.f, 10.f);
+	for (float i = -5.f; i < 15.f; i += 10.f) {
+		glPushMatrix();
+		glTranslatef(13.f, -2.f, i);
+		glColor4f(0.f, 0.f, 0.f, 1.f);
+		glutSolidSphere(2.f, 10.f, 10.f);
+		glPopMatrix();
+	}
+	glPopMatrix(); // 1
+
+	glPushMatrix();
+	glTranslatef(0.f, -50.f, 0.f);
+	glScalef(15.f, 40.f, 50.f);
+	glColor4f(0.7f, 0.f, 0.f, 1.f);
+	glutSolidCube(1);
+	glPopMatrix();
+
+	for (float i = -15.f; i < 30.f; i += 30.f) {
+		glColor4f(0.f, 0.f, 0.7f, 1.f);
+		glPushMatrix();
+		glTranslatef(0.f, -80.f, i);
+		glScalef(10.f, 40.f, 10.f);
+		glutSolidCube(1);
+		glPopMatrix();
+	}
+	glPopMatrix(); // »ç¶÷
 
 	glPopMatrix(); // x_z ÁÂÇ¥ÀÌµ¿
 }
